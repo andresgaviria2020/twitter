22d5db7a28b4fcaea14d66e3b36939de42618a39
Andrés Gaviria <andres_felipe_gaviria28@hotmail.com>
2021-04-24 14:08:26 -0500

    Conexión a bd mongo
    Conexión a bd mongo

---
+++ bd/conexionBD.go
@@ -0,0 +1,37 @@
+package bd
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var MongoCN = ConectarBD()
+var clientOptions = options.Client().ApplyURI("url_conection")
+
+/*ConcectarBD */
+func ConectarBD() *mongo.Client {
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err.Error())
+		return client
+	}
+	log.Println("Conexión Exitosa con la bd")
+	return client
+}
+
+/*ChequeoConnection */
+func ChequeoConnection() int {
+	err := MongoCN.Ping(context.TODO(), nil)
+	if err != nil {
+		return 0
+	}
+	return 1
+}
